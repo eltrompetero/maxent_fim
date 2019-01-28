ad80837e66a65fb19512415532b8430fcc6462f0
Eddie Lee <el.trompetero@gmail.com>
2019-01-28 15:31:12 -0500

    added perturbation away from neighbor aStar in Method2

--- utils.py
+++ utils.py
@@ -691,7 +691,7 @@ class IsingFisherCurvatureMethod2(IsingFisherCurvatureMethod1):
                 self.dJ[counter], errflag = self.solve_linearized_perturbation(i, a)
                 counter += 1
 
-    def observables_after_perturbation(self, i, a, eps=None):
+    def observables_after_perturbation(self, i, a, eps=None, perturb_up=False):
         """Make spin index i more like spin a by eps. Perturb the corresponding mean and
         the correlations with other spins j.
         
@@ -702,6 +702,7 @@ class IsingFisherCurvatureMethod2(IsingFisherCurvatureMethod1):
         a : int
             Spin to mimic.
         eps : float, None
+        perturb_up : bool, False
 
         Returns
         -------
@@ -725,13 +726,18 @@ class IsingFisherCurvatureMethod2(IsingFisherCurvatureMethod1):
         # observables after perturbations
         siNew = si.copy()
         sisjNew = sisj.copy()
+        
+        if perturb_up:
+            for i_,a_,eps_ in zip(i,a,eps):
+                self._observables_after_perturbation_up(siNew, sisjNew, i_, a_, eps_)
+        else:
+            for i_,a_,eps_ in zip(i,a,eps):
+                self._observables_after_perturbation_down(siNew, sisjNew, i_, a_, eps_)
 
-        for i_,a_,eps_ in zip(i,a,eps):
-            self._observables_after_perturbation(siNew, sisjNew, i_, a_, eps_)
 
         return np.concatenate((siNew, sisjNew))
    
-    def _observables_after_perturbation(self, si, sisj, i, a, eps):
+    def _observables_after_perturbation_up(self, si, sisj, i, a, eps):
         n = self.n
 
         si[i] = (1-eps)*si[i] + eps*si[a]
@@ -750,6 +756,26 @@ class IsingFisherCurvatureMethod2(IsingFisherCurvatureMethod1):
                 else:
                     jaix = unravel_index((a,j),n)
                 sisj[ijix] = (1-eps)*sisj[ijix] + eps*sisj[jaix]
+
+    def _observables_after_perturbation_down(self, si, sisj, i, a, eps):
+        n = self.n
+
+        si[i] = (1-eps)*si[i] - eps*si[a]
+
+        for j in np.delete(range(n),i):
+            if i<j:
+                ijix = unravel_index((i,j),n)
+            else:
+                ijix = unravel_index((j,i),n)
+
+            if j==a:
+                sisj[ijix] = (1-eps)*sisj[ijix] - eps
+            else:
+                if j<a:
+                    jaix = unravel_index((j,a),n)
+                else:
+                    jaix = unravel_index((a,j),n)
+                sisj[ijix] = (1-eps)*sisj[ijix] - eps*sisj[jaix]
 
     def _solve_linearized_perturbation(self, iStar, aStar):
         """Consider a perturbation to a single spin.
