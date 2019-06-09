a1df289867fadb12ec717297ee19f2a1f01f777b
Eddie Lee <el.trompetero@gmail.com>
2019-06-09 15:45:52 -0400

    added setup files for running project

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: Eddie Lee, edl56@cornell.edu
+echo "Open the file and read instructions."
+
+# Run the following commands in your shell prompt.
+# conda create --name <env_name>  --file requirements.txt
+# conda activate <env_name>
+# pip install coniii
+# git clone https://github.com/eltrompetero/workspace.git
+# git clone https://github.com/eltrompetero/scotus3_pyutils.git
