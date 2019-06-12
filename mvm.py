b38e4656fcc0aea32a758976d6e0e4b9c6882fbd
Eddie Lee <el.trompetero@gmail.com>
2019-06-12 10:39:17 -0400

    added mvm.setup_mvm_with_perturbation()

--- mvm.py
+++ mvm.py
@@ -40,3 +40,121 @@ def create_mvm_p(n, q):
         ix = allStates[pmvm>0][:,0]==np.sign(allStates[pmvm>0][:,1:].sum(1))
         assert (np.sign(allStates[pmvm>0][ix==0][:,1:].sum(1))==0).all()
     return pmvm
+
+def setup_mvm_with_perturbation(n):
+    """Median Voter Model with special Ordinary voter O' that has special couplings with
+    the Median and the remaining O voters.
+    
+    Check formulation in SCOTUS II pg. 116.
+    
+    Parameters
+    ----------
+    int : n
+    
+    Returns
+    -------
+    function
+        smo(Jm, Jmp, Jo, Jop)
+    function
+        smo_prime(Jm, Jmp, Jo, Jop)
+    function
+        soo(Jm, Jmp, Jo, Jop)
+    function
+        soo_prime(Jm, Jmp, Jo, Jop)
+    """
+    
+    _E_with_maj_with_median = lambda Jm,Jmp,Jo,Jop,k,n=n: -(Jm*(2*k-n-2) +
+                                          Jmp +
+                                          Jo*(binom(k-2,2)+binom(n-k,2)-(k-2)*(n-k)) +
+                                          Jop*(2*k-n-2))
+    _E_with_maj_against_median = lambda Jm,Jmp,Jo,Jop,k,n=n: -(Jm*(2*k-n) -
+                                           Jmp +
+                                           Jo*(binom(k-1,2)+binom(n-k-1,2)-(k-1)*(n-k-1)) +
+                                           Jop*(n-2*k))
+    _E_not_with_maj_with_median = lambda Jm,Jmp,Jo,Jop,k,n=n: -(Jm*(n-2*k-2) +
+                                          Jmp +
+                                          Jo*(binom(n-k-2,2)+binom(k,2)-(n-k-2)*k) +
+                                          Jop*(n-2*k-2))
+    _E_not_with_maj_against_median = lambda Jm,Jmp,Jo,Jop,k,n=n: -(Jm*(n-2*k) -
+                                            Jmp +
+                                            Jo*(binom(n-k-1,2)+binom(k-1,2)-(n-k-1)*(k-1)) +
+                                            Jop*(2*k-n))
+    
+    Z = lambda Jm,Jmp,Jo,Jop,n=n:( sum([binom(n,k) * 
+           (k/n * ((k-1)/(n-1) * np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) +
+                   (n-k)/(n-1) * np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,k))) +
+           (n-k)/n * ((n-k-1)/(n-1) * np.exp(-_E_not_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) + 
+                      k/(n-1) * np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,k))))
+                            for k in range(n//2+1,n-1)]) + 
+           n*((n-1)/n * ((n-2)/(n-1) * np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n-1)) +
+                   1/(n-1) * np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) +
+                   1/n * np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) + 
+           np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n)))
+    
+    # <s_Median s_Ordinary>
+    def smo(Jm, Jmp, Jo, Jop, n=n):
+        return (sum([binom(n,k) * 
+           (k/n * ((k-1)/(n-1) * ((k-2)/(n-2) - (n-k)/(n-2)) * 
+                                   np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) +
+                   (n-k)/(n-1) * ((k-1)/(n-2) - (n-k-1)/(n-2)) * 
+                                   np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,k))) +
+           (n-k)/n * ((n-k-1)/(n-1) * ((n-k-2)/(n-2) - k/(n-2)) * 
+                                  np.exp(-_E_not_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) + 
+                      k/(n-1) * ((n-k-1)/(n-2) - (k-1)/(n-2)) * 
+                                  np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,k))))
+                            for k in range(n//2+1,n-1)]) + 
+            n*((n-1)/n * ((n-2)/(n-1) * ((n-3)/(n-2) - 1/(n-2)) *
+                                  np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n-1)) +
+                   1/(n-1) * np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) +
+                   -1/n * np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) + 
+            np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n))) / Z(Jm,Jmp,Jo,Jop)
+    
+    # <s_M s_O'>
+    def smop(Jm, Jmp, Jo, Jop, n=n):
+        return (sum([binom(n,k) * 
+           (k/n * ((k-1)/(n-1) * np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) +
+                   (n-k)/(n-1) * -np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,k))) +
+           (n-k)/n * ((n-k-1)/(n-1) * np.exp(-_E_not_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) + 
+                      k/(n-1) * -np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,k))))
+                            for k in range(n//2+1,n-1)]) + 
+            n*((n-1)/n * ((n-2)/(n-1) * np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n-1)) +
+                   1/(n-1) * -np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) +
+               1/n * ( -np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1)))) + 
+            np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n))) / Z(Jm,Jmp,Jo,Jop)
+    
+    # <s_O s_O''>
+    def soo(Jm, Jmp, Jo, Jop, n=n):
+        return (sum([binom(n,k) * 
+           (k/n * ((k-1)/(n-1) * (binom(k-2,2)+binom(n-k,2)-(k-2)*(n-k))/binom(n-2,2) * 
+                                   np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) +
+                   (n-k)/(n-1) * (binom(k-1,2)+binom(n-k-1,2)-(k-1)*(n-k-1))/binom(n-2,2) * 
+                                   np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,k))) +
+           (n-k)/n * ((n-k-1)/(n-1) * (binom(n-k-2,2)+binom(k,2)-(n-k-2)*k)/binom(n-2,2) * 
+                                  np.exp(-_E_not_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) + 
+                      k/(n-1) * (binom(n-k-1,2)+binom(k-1,2)-(n-k-1)*(k-1))/binom(n-2,2) * 
+                                  np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,k))))
+                            for k in range(n//2+1,n-1)]) + 
+           n*((n-1)/n * ((n-2)/(n-1) * (binom(n-3,2)-(n-3))/binom(n-2,2) * 
+                                   np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n-1)) +
+                   1/(n-1) * np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) +
+               1/n * np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) +
+            np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n))) / Z(Jm,Jmp,Jo,Jop)
+    
+    # <s_O s_O'>
+    def sop(Jm, Jmp, Jo, Jop, n=n):
+        return (sum([binom(n,k) * 
+       (k/n * ( (k-1)/(n-1) * ((k-2)/(n-2) - (n-k)/(n-2)) * 
+                       np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) +
+                (n-k)/(n-1) * ((n-k-1)/(n-2) - (k-1)/(n-2)) * 
+                       np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,k)) ) +
+       (n-k)/n * ( (n-k-1)/(n-1) * ((n-k-2)/(n-2) - k/(n-2)) * 
+                       np.exp(-_E_not_with_maj_with_median(Jm,Jmp,Jo,Jop,k)) + 
+                k/(n-1) * ((k-1)/(n-2) - (n-k-1)/(n-2)) * 
+                       np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,k))))
+                            for k in range(n//2+1,n-1)]) + 
+       n*((n-1)/n * ( (n-2)/(n-1) * ((n-3)/(n-2) - 1/(n-2)) * 
+                       np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n-1)) +
+                      -1/(n-1) * np.exp(-_E_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1)) ) +
+          1/n * np.exp(-_E_not_with_maj_against_median(Jm,Jmp,Jo,Jop,n-1))) +
+       np.exp(-_E_with_maj_with_median(Jm,Jmp,Jo,Jop,n))) / Z(Jm,Jmp,Jo,Jop)
+    return smo, smop, soo, sop, Z
