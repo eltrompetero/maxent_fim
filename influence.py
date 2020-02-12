17170618034e180ac516478c726801bd761c0023
Eddie Lee <el.trompetero@gmail.com>
2020-02-11 17:24:51 -0700

    simplified interface for influence fcns

--- influence.py
+++ influence.py
@@ -5,15 +5,13 @@
 from .utils import *
 
 
-def block_subspace_eig(hess, k, remove_n_modes=0):
+def block_subspace_eig(hess, remove_n_modes=0):
     """Spectral analysis of diagonal blocks in the FIM that correspond to individual
     subspaces.
 
     Parameters
     ----------
     hess : ndarray
-    k : int
-        Size of each diagonal block.
     remove_n_modes: int, 0
         Number of modes to subtract off the FIM before extracting the blocks.
 
@@ -25,11 +23,16 @@ def block_subspace_eig(hess, k, remove_n_modes=0):
         Eigenvectors for each block.
     """
     
-    assert (len(hess)%k)==0
+    n = (1+np.sqrt(1+4*hess.shape[0])) / 2
+    assert int(n)==n, "Cannot be reshaped into n,n pairwise matrix with zeroed diagonal."
+    n = int(n)
+    k = n-1
+
     if remove_n_modes>0:
         for i in range(remove_n_modes):
             hess = remove_principal_mode(hess)
     
+    # for each spin subspace, perform spectral analysis
     blockeigval = []
     blockeigvec = []
     
@@ -44,16 +47,14 @@ def block_subspace_eig(hess, k, remove_n_modes=0):
         blockeigvec.append(v)
     return blockeigval, blockeigvec
 
-def pair_asymmetry(eigvec, n, rank=0, by_voter=False, eigval=None):
-    """Row column asymmetry for "eigenmatrices" for pairwise perturbations. Can calculated
+def pair_asymmetry(eigvec, rank=0, by_voter=False, eigval=None):
+    """Row column asymmetry for "eigenmatrices" for pairwise perturbations. Can calculate
     weighted asymmetry if eigenvalues are passed in.
 
     Parameters
     ----------
     eigvec : ndarray
         Matrix of eigenvectors where each col is an eigenvector.
-    n : int
-        System size.
     rank : int, 0
         Column of eigvec array to use corresponding to the rank of the eigenvector.
     by_voter : bool, False
@@ -69,7 +70,7 @@ def pair_asymmetry(eigvec, n, rank=0, by_voter=False, eigval=None):
     """
     
     if eigval is None:
-        v = np.insert(eigvec[:,rank], range(0,n*n,n), 0).reshape(n,n)
+        v = vec2mat(eigvec[:,rank])
         if by_voter:
             return ((v-v.T)**2).sum(0)/4
             # old measure
@@ -81,7 +82,7 @@ def pair_asymmetry(eigvec, n, rank=0, by_voter=False, eigval=None):
     # remains to convert the following calculating of asymmetry to the latest definition
     a = 0.
     for rank, val in enumerate(eigval):
-        v = np.insert(eigvec[:,rank], range(0,n*n,n), 0).reshape(n,n)
+        v = vec2mat(eigvec[:,rank])
         if by_voter:
             a += (v**2+v.T**2-2*np.abs(v*v.T)).sum(0)/2 * val**2
         else:
