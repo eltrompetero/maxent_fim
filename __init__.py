a8a8ed73f576f767ecd13c2afb0e416c8a1f913e
Eddie Lee <el.trompetero@gmail.com>
2020-05-16 08:41:59 -0600

    improvements to organizer.MESolution

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
+# ====================================================================================== #
+# Information geometry of social and biological networks.
+# Author: Eddie Lee, edlee@santafe.edu
+# ====================================================================================== #
 from .utils import *
-from . import pyfim
+from .influence import *
+from . import fim as pyfim
 from . import large_fim as lpyfim
 from .organizer import MESolution
--- organizer.py
+++ organizer.py
@@ -10,7 +10,7 @@ import os
 class MESolution():
     DEFAULT_DR = 'cache/c_elegans'
 
-    def __init__(self, name, ix):
+    def __init__(self, name, ix, iprint=True):
         """
         Parameters
         ----------
@@ -22,11 +22,31 @@ class MESolution():
         self.ix = ix
         
         files = os.listdir(self.DEFAULT_DR)
-        if not '%s%d.p'%(name, ix) in files:
-            raise Exception("Maxent solution file not found.")
-        self.n = pickle.load(open('%s/%s%d.p'%(self.DEFAULT_DR, name, ix), 'rb'))['X'].shape[1]
+
+        # look for maxent soln results
+        if not '%s_soln%d.p'%(name, ix) in files:
+            if iprint: print("Maxent solution file not found.")
+            if not '%s_model%d.p'%(name, ix) in files:
+                raise Exception("Neither maxent nor model file found.")
+            else:
+                self.n = pickle.load(open('%s/%s_model%d.p'%(self.DEFAULT_DR, name, ix), 'rb'))['n']
+                self._model = True
+            self._me = False
+        else:
+            self.n = pickle.load(open('%s/%s_soln%d.p'%(self.DEFAULT_DR, name, ix), 'rb'))['X'].shape[1]
+            if not '%s_model%d.p'%(name, ix) in files:
+                if iprint: print("Model file not found.")
+                self._model = False
+            else:
+                self._model = True
+            self._me = True
+        
+        # look for FIM results
         if not '%s_fim%d.p'%(name, ix) in files:
-            print("FIM not found.")
+            if iprint: print("FIM not found.")
+            self._fim = False
+        else:
+            self._fim = True
 
     def parameters(self):
         """Get maxent solution for specified data set.
@@ -39,14 +59,22 @@ class MESolution():
             Couplings.
         """
         
-        fname = '%s%d.p'%(self.name, self.ix)
+        if self._me:
+            fname = '%s_soln%d.p'%(self.name, self.ix)
+        else:
+            fname = '%s_model%d.p'%(self.name, self.ix)
         indata = pickle.load(open('%s/%s'%(self.DEFAULT_DR, fname), 'rb'))
+
         if 'h' in indata.keys() and 'J' in indata.keys():
             return indata['h'], indata['J']
         elif 'hJ' in indata.keys():
             return indata['hJ'][:3*self.n], indata['hJ'][3*self.n:]
         else:
             raise Exception("Could not find parameters in data pickle.")
+    
+    def model(self):
+        from .utils import load_Coupling3
+        return load_Coupling3('%s/%s_model%d.p'%(self.DEFAULT_DR, self.name, self.ix))
 
     def fim(self):
         """
@@ -55,6 +83,6 @@ class MESolution():
         ndarray
         """
 
-        fname = '%s_fim%d.p'%(self.fim, self.ix)
+        fname = '%s_fim%d.p'%(self.name, self.ix)
         return pickle.load(open('%s/%s'%(self.DEFAULT_DR, fname), 'rb'))['fim']
 #end MESolution
